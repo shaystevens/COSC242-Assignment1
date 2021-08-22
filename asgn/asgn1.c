b5e23c1bb2b6159571cccfbce33dcb050e9036ff
mwong <wonma004@student.otago.ac.nz>
2021-08-22 16:23:21 +1200

    Modified to use default capacity if no command line argument

--- asgn/asgn1.c
+++ asgn/asgn1.c
@@ -7,19 +7,21 @@ int main(int argc, char **argv) {
     int capacity;
     htable h;
     char word[256];
-    /*char words;*/
+    const int default_capacity = 113;
 
     if (argc > 1){
         sscanf(argv[1], "%d", &capacity);
-        h = htable_new(capacity);
-
-        while (getword(word, sizeof word, stdin) != EOF) {
-            htable_insert(h, word);
-        }
-
-        htable_print(h, stdout);
-        htable_free(h);        
+        h = htable_new(capacity);      
+    } else {
+        h = htable_new(default_capacity);
     }
 
+    while (getword(word, sizeof word, stdin) != EOF) {
+        htable_insert(h, word);
+    }
+
+    htable_print(h, stdout);
+    htable_free(h);  
+    
     return EXIT_SUCCESS;
 }
