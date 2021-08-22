119264b2d189ec67dbf31827375cf2e678657884
mwong <wonma004@student.otago.ac.nz>
2021-08-22 16:15:46 +1200

    Removed excess comments

--- Lab10Madison/htable-program.c
+++ Lab10Madison/htable-program.c
@@ -4,23 +4,13 @@
 #include "htable.h"
 
 int main(int argc, char **argv) {
-    /*int item;
-    htable my_htable = htable_new(11);
-
-    htable_free(my_htable);*/
-
     int capacity;
     htable h;
     char word[256];
     /*char words;*/
 
     if (argc > 1){
-        /* capacity = (int)argv[1]; FIX!!!!! at number 2*/
-        /*printf("%s", argv[1]);*/
         sscanf(argv[1], "%d", &capacity);
-        /* printf("%d\n", capacity);*/
-    
-
         h = htable_new(capacity);
 
         while (getword(word, sizeof word, stdin) != EOF) {
@@ -28,9 +18,7 @@ int main(int argc, char **argv) {
         }
 
         htable_print(h, stdout);
-        htable_free(h);
-
-        
+        htable_free(h);        
     }
 
     return EXIT_SUCCESS;
