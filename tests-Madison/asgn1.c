9429360d49a612608f90cc770f02369a1d978ba7
sstevens <stesh969@student.otago.ac.nz>
2021-08-24 15:12:37 +1200

    Implemented -h -p and -s

--- tests-Madison/asgn1.c
+++ tests-Madison/asgn1.c
@@ -8,6 +8,25 @@ static void print_info(int freq, char *word) {
     printf("%-4d %s\n", freq, word);
 }
 
+static void help_message(){
+    printf("Usage: ./asgn [OPTION]... <STDIN>\n\n");
+
+    printf("Perform various operations using a hash table.  By default, words are\n" 
+           "read from stdin and added to the hash table, before being printed out\n"
+           "alongside their frequencies to stdout.\n\n");
+
+    printf(" -d           Use double hashing (linear probing is the default)\n"
+           " -e           Display entire contents of hash table on stderr\n"
+           " -p           Print stats info instead of frequencies & words\n"
+           " -s SNAPSHOTS Show SNAPSHOTS stats snapshots (if -p is used)\n"
+           " -t TABLESIZE Use the first prime >= TABLESIZE as htable size\n"
+           "\n"
+           " -h           Display this message\n\n");
+
+    exit(EXIT_SUCCESS);
+           
+}
+    
 static int isPrime(int i){
     int x;
     for(x=2; x <= i/2; x++){
@@ -40,9 +59,10 @@ int main(int argc, char **argv) {
     const char *optstring = "deps:t:h";
     char option;
     /* int is_Double_H = 0; 1 for yes, 0 for no*/
-    int options[5] = {0, 0, 0, 0, 0}; /* {0 = linear 1 = double, e = 1 option run, p =
-                                       1 option run, s = how many snapshots, 1 = tablesize set*/
-    if(arc > 0){
+    int options[5] = {0, 0, 0, 10, 0};
+    /* {0 = linear 1 = double, e = 1 option run, p = 1 option run, s = how many snapshots, 1 = tablesize set*/
+    
+    if(argc > 0){
         while ((option = getopt(argc, argv, optstring)) != EOF) {
             switch (option) {
                 case 'd':
@@ -71,7 +91,7 @@ int main(int argc, char **argv) {
                     break;
 
                 case 's':
-                    options[3] = sscanf(optarg, "%d", &options[3]);
+                    options[3] = atoi(optarg);
                     break;
 
                 case 't':
@@ -82,7 +102,7 @@ int main(int argc, char **argv) {
                     /* h = htable_new(prime_greater(capacity), DOUBLE_H); */
                 case 'h':
                
-                    fprintf(stdout, "help info");
+                    help_message();
                     /*"Perform various operations using a hash table.  By default, \
                       words are read from stdin and added to the hash table, before being printed out\
                       alongside their frequencies to stdout.
@@ -98,7 +118,7 @@ int main(int argc, char **argv) {
                 
                 default:
                     /* if an unknown option is given */
-                    fprintf(stdout, "help info invalid option"); /*"Perform various operations using a hash table.  By default, words are\n read from stdin and added to the hash table, before being printed out\n
+                    help_message(); /*"Perform various operations using a hash table.  By default, words are\n read from stdin and added to the hash table, before being printed out\n
                                                                    alongside their frequencies to stdout.\n\n
 
                                                                    -d           Use double hashing (linear probing is the default)\n
@@ -108,7 +128,7 @@ int main(int argc, char **argv) {
                                                                    -t TABLESIZE Use the first prime >= TABLESIZE as htable size\n
 
                                                                    -h           Display this message\n");*/
-                    return EXIT_SUCCESS;
+                    
             }
         }
     }
@@ -131,9 +151,12 @@ int main(int argc, char **argv) {
         htable_print_entire_table(h, stderr);
     }
 
+    if(options[3] <= 0){
+        options[3] = 10;
+    }
+
     if (options[2] == 1){
-        /* if 
-           htable_print_stats(h, stdout, options[3])*/
+        htable_print_stats(h, stdout, options[3]);
 
         /*must link p and s, divide 100 percent into increments */
     } else {
