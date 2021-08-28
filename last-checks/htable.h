3c36fe0ceda9e5e87ecab6184b99327a24c3946b
mwong <wonma004@student.otago.ac.nz>
2021-08-29 03:01:45 +1200

    Deleted search and erealloc and fixed the print entire table in regards to print info

---
+++ last-checks/htable.h
@@ -0,0 +1,16 @@
+#ifndef HTABLE_H_
+#define HTABLE_H_
+
+#include <stdio.h>
+
+typedef struct htablerec *htable;
+typedef enum hashing_e { LINEAR_P, DOUBLE_H } hashing_t;
+
+extern void   htable_free(htable h);
+extern int    htable_insert(htable h, char *str);
+extern htable htable_new(int capacity, hashing_t hashing_type);
+extern void   htable_print(htable h, void print_info(int freq, char *word));
+extern void   htable_print_entire_table(htable h, FILE *stream);
+extern void   htable_print_stats(htable h, FILE *stream, int num_stats);
+
+#endif
